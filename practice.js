a12674230f647391f4db2e40d53a1ac8087fe37b
ehaugen5 <ehaugen@bowdoin.edu>
2022-11-11 10:00:42 -0500

    practice trial

---
+++ practice.js
@@ -0,0 +1,45 @@
+var practice = 
+[
+    {
+      "Sentence": "You drop a brick on a banana.",
+      "Condition": "Typical_Smashed",
+      "Correct": "S",
+      "Image": "bananaS.jpeg",
+      "List": 1
+    },
+      {
+        "Sentence": "You drop a brick on a mango.",
+        "Condition": "Atypical_Smashed",
+        "Correct": "S",
+        "Image": "mangoAS.jpeg",
+        "List": 1
+      },
+      {
+        "Sentence": "You drop a brick on a strawberry.",
+        "Condition": "Typical_Intact",
+        "Correct": "N",
+        "Image": "strawberryN.jpg",
+        "List": 1
+      },
+      {
+        "Sentence": "You drop a brick on a chocolate.",
+        "Condition": "Typical_Smashed",
+        "Correct": "S",
+        "Image": "chocolatebarS.jpg",
+        "List": 1
+      },
+      {
+        "Sentence": "You drop a brick on a kiwi.",
+        "Condition": "Atypical_Intact",
+        "Correct": "N",
+        "Image": "kiwiAN.jpeg",
+        "List": 1
+      },
+      {
+        "Sentence": "You drop a brick on an orange.",
+        "Condition": "Atypical_Smashed",
+        "Correct": "S",
+        "Image": "orangeAS.JPG",
+        "List": 1
+      }
+]
