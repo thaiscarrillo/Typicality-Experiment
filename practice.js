68c306a38413daba83e47313db393bd30cebf0b7
thaiscarrillo <tcarrill@bowdoin.edu>
2022-11-15 09:55:50 -0500

    new practice

--- practice.js
+++ practice.js
@@ -1,45 +1,45 @@
-var practice = 
-[
-    {
-      "Sentence": "You drop a brick on a banana.",
-      "Condition": "Typical_Smashed",
-      "Correct": "S",
-      "Picture": "bananaS.jpeg",
-      "List": 1
-    },
-      {
-        "Sentence": "You drop a brick on a mango.",
-        "Condition": "Atypical_Smashed",
-        "Correct": "S",
-        "Picture": "mangoAS.jpeg",
-        "List": 1
-      },
-      {
-        "Sentence": "You drop a brick on a strawberry.",
-        "Condition": "Typical_Intact",
-        "Correct": "N",
-        "Picture": "strawberryN.jpg",
-        "List": 1
-      },
-      {
-        "Sentence": "You drop a brick on a chocolate.",
-        "Condition": "Typical_Smashed",
-        "Correct": "S",
-        "Picture": "chocolatebarS.jpg",
-        "List": 1
-      },
-      {
-        "Sentence": "You drop a brick on a kiwi.",
-        "Condition": "Atypical_Intact",
-        "Correct": "N",
-        "Picture": "kiwiAN.jpeg",
-        "List": 1
-      },
-      {
-        "Sentence": "You drop a brick on an orange.",
-        "Condition": "Atypical_Smashed",
-        "Correct": "S",
-        "Picture": "orangeAS.JPG",
-        "List": 1
-      }
-]
+var practice =
+    [
+        {
+            "Sentence": "You drop a brick on a banana.",
+            "Condition": "Practice",
+            "Correct": "S",
+            "Picture": "bananaS.jpeg",
+            "List": 1
+        },
+        {
+            "Sentence": "You drop a brick on a mango.",
+            "Condition": "Practice",
+            "Correct": "S",
+            "Picture": "mangoAS.jpeg",
+            "List": 1
+        },
+        {
+            "Sentence": "You drop a brick on a strawberry.",
+            "Condition": "Practice",
+            "Correct": "N",
+            "Picture": "strawberryN.jpg",
+            "List": 1
+        },
+        {
+            "Sentence": "You drop a brick on a chocolate.",
+            "Condition": "Practice",
+            "Correct": "S",
+            "Picture": "chocolatebarS.jpg",
+            "List": 1
+        },
+        {
+            "Sentence": "You drop a brick on a kiwi.",
+            "Condition": "Practice",
+            "Correct": "N",
+            "Picture": "kiwiAN.jpeg",
+            "List": 1
+        },
+        {
+            "Sentence": "You drop a brick on an orange.",
+            "Condition": "Practice",
+            "Correct": "S",
+            "Picture": "orangeAS.JPG",
+            "List": 1
+        }
+    ]
